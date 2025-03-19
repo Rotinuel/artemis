25f30484b9f5b0487f2b00483312a0593bcb8a87
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2025-03-19 10:12:02 +0100

    second commit

---
+++ app/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>Header</div>
+  )
+}
+
+export default Header
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import Image from "next/image";
 import { assets } from "../assets/assets";
+import { useRef } from "react";
 
 const Navbar = () => {
+
+    const sideMenuRef = useRef()
+
+    const openMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(-16rem)'
+    }
+
+    const closeMenu = () => {
+        sideMenuRef.current.style.transform = 'translateX(16rem)'
+    }
+
   return (
     <>
     <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
@@ -28,12 +40,43 @@ const Navbar = () => {
             <a className="font-ovo" href="#top">Contact Us</a>
           </li>
         </ul>
-        <div>
+        <div className="flex items-center gap-4">
+            <button>
+                <Image src={assets.moon_icon} alt='' className="w-6"/>
+            </button>
           <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-ovo">
             contact
             <Image src={assets.arrow_icon} alt='' className='w-3'/>
           </a>
+          <button className="block md:hidden ml-3" onClick={openMenu}>
+                <Image src={assets.menu_black} alt='' className="w-6"/>
+            </button>
         </div>
+
+        {/* mobile menu */}
+        <ul ref={sideMenuRef} className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-alGreen transition duration-500">
+            <div className="absolute right-6 top-6" onClick={closeMenu}>
+                <Image src={assets.close_black} alt='' className="w-5 cursor-pointer"/>
+            </div>
+          <li>
+            <a className="font-ovo" href="#top" onClick={closeMenu}>Home</a>
+          </li>
+          <li>
+            <a className="font-ovo" href="#top" onClick={closeMenu}>About Us</a>
+          </li>
+          <li>
+            <a className="font-ovo" href="#top" onClick={closeMenu}>Our Services</a>
+          </li>
+          <li>
+            <a className="font-ovo" href="#top" onClick={closeMenu}>Our Projects</a>
+          </li>
+          <li>
+            <a className="font-ovo" href="#top" onClick={closeMenu}>Contact Us</a>
+          </li>
+        </ul>
+
+
+
       </nav>
     </>
   );
