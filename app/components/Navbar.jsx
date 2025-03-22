05d1283b4200bf167320d84c9ff0b3e379447221
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2025-03-22 18:47:50 +0100

    logo adjustment

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -28,9 +28,9 @@ const Navbar = () => {
     <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
         <Image src={assets.header_bg_color} alt='' className='w-full'/>
     </div>
-      <nav className={`fixed w-full px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScrolled ? "bg-white shadow-md" : "bg-transparent"} md:bg-transparent`}>
+      <nav className={`fixed w-full px-5 lg:px-8 xl:px-[8%] py-1 flex items-center justify-between z-50 ${isScrolled ? "bg-white shadow-md" : "bg-transparent"} md:bg-transparent`}>
         <a href="#top" className='w-28 cursor-pointer mr-14'>
-          <Image src={assets.logobg} alt="logo" className='w-28 cursor-pointer mr-14'/>
+          <Image src={assets.logobg} alt="logo" className='w-18 cursor-pointer mr-14'/>
         </a>
         <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50">
           <li>
