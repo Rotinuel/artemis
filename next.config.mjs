ff689e443c8fce5bd112787937e7299cc03930f9
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2025-12-19 13:59:47 +0100

    Disable video asset handling in next.config.mjs
    Comment out webpack configuration for video asset handling.

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,13 @@ import withVideos from 'next-videos';
 /** @type {import('next').NextConfig} */
 const nextConfig = withVideos({
     turbopack: false,
-    webpack(config) {
-        config.module.rules.push({
-          test: /\.(mp4|webm|ogg|swf|ogv)$/,
-          type: 'asset/resource',
-        });
-        return config;
-    },
+    // webpack(config) {
+    //     config.module.rules.push({
+    //       test: /\.(mp4|webm|ogg|swf|ogv)$/,
+    //       type: 'asset/resource',
+    //     });
+    //     return config;
+    // },
 });
 
 export default nextConfig;
