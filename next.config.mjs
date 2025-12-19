c42c857ecbfb72b6ef72aef77073fea0b7207e5b
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2025-12-19 13:26:02 +0100

    Disable turbopack in Next.js configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import withVideos from 'next-videos';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withVideos({
+    turbopack: false,
     webpack(config) {
         config.module.rules.push({
           test: /\.(mp4|webm|ogg|swf|ogv)$/,
